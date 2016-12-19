b0b92428d5d4e373db15afdbd1ef4bf10aec68ac
einaralex <einaralex@gmail.com>
2016-12-19 01:58:36 +0000

    clean up

---
+++ provisioning/initinstance.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+scp -o StrictHostKeyChecking=no -i "hgop-key-pair.pem" ./docker-compose.yaml ec2-user@ec2-52-214-66-118.eu-west-1.compute.amazonaws.com:~/docker-compose.yaml
+scp -o StrictHostKeyChecking=no -i "hgop-key-pair.pem" ./.env ec2-user@ec2-52-214-66-118.eu-west-1.compute.amazonaws.com:~/.env
+
+
+ssh -i "hgop-key-pair.pem" ec2-user@ec2-52-214-66-118.eu-west-1.compute.amazonaws.com
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+exit
--- provisioning/newinstance.sh
+++ provisioning/newinstance.sh
@@ -1,20 +1,2 @@
-#!/bin/bash
-
-scp -o StrictHostKeyChecking=no -i "hgop-key-pair.pem" ./docker-compose.yaml ec2-user@ec2-52-214-66-118.eu-west-1.compute.amazonaws.com:~/docker-compose.yaml
-scp -o StrictHostKeyChecking=no -i "hgop-key-pair.pem" ./.env ec2-user@ec2-52-214-66-118.eu-west-1.compute.amazonaws.com:~/.env
-
-
-ssh -i "hgop-key-pair.pem" ec2-user@ec2-52-214-66-118.eu-west-1.compute.amazonaws.com
-sudo yum update -y
-sudo yum install -y docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-
-curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-exit
-
-ssh -i "hgop-key-pair.pem" ec2-user@ec2-52-214-66-118.eu-west-1.compute.amazonaws.com
-
-docker run -d -p 80:8080 einaralex/tictactoe:latest node run.js
+INSTANCE_ID=$(aws ec2 run-instances  --user-data file://initinstance.sh --image-id ami-9398d3e0 --security-group-ids 	sg-6cde530a --count 1 --instance-type t2.micro --key-name launch-wizard-1 --query 'Instances[0].InstanceId'  --output=text)
+echo ${INSTANCE_ID} > .provisioning/instance-id.txt
